1076ae519bdc76f34c076fb112519f0b21b280af
Cogi <935462955@qq.com>
2023-04-02 03:53:10 +0800

    Update app.ts
    取消默认最小token限制

--- app/store/app.ts
+++ app/store/app.ts
@@ -134,7 +134,7 @@ const DEFAULT_CONFIG: ChatConfig = {
   modelConfig: {
     model: "gpt-3.5-turbo",
     temperature: 1,
-    max_tokens: 2000,
+    max_tokens: 1000000000000000000,
     presence_penalty: 0,
   },
 };
